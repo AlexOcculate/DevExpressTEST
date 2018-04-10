90c83dc7d493f1273e2718f3d46f80410b51d5aa
AM O <occulate@yahoo.com>
2018-04-10 10:22:53 +1000

    W.I.P.2

---
+++ Prop/PropXtraUserControl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace Prop
+{
+   public partial class PropXtraUserControl : DevExpress.XtraEditors.XtraUserControl
+   {
+      public PropXtraUserControl()
+      {
+         InitializeComponent( );
+      }
+
+      internal void SetSelectedObject( object o )
+      {
+         this.propertyGridControl1.SelectedObject = o;
+      }
+
+      private void sortOrderBarButtonItem_ItemClick( object sender, DevExpress.XtraBars.ItemClickEventArgs e )
+      {
+
+      }
+
+      private void expandBarButtonItem_ItemClick( object sender, DevExpress.XtraBars.ItemClickEventArgs e )
+      {
+
+
+      }
+   }
+}
