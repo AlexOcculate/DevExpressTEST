b4d363706d93e88a912c53cd1bdd9935a2a5acb8
AM O <occulate@yahoo.com>
2018-04-16 14:33:43 +1000

    DockManagerTest: W.I.P.

---
+++ DockManagerTest/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace DockManagerTest
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose( bool disposing )
+      {
+         if( disposing && (components != null) )
+         {
+            components.Dispose( );
+         }
+         base.Dispose( disposing );
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.components = new System.ComponentModel.Container();
+         this.barManager1 = new DevExpress.XtraBars.BarManager(this.components);
+         this.bar1 = new DevExpress.XtraBars.Bar();
+         this.bar2 = new DevExpress.XtraBars.Bar();
+         this.bar3 = new DevExpress.XtraBars.Bar();
+         this.barDockControlTop = new DevExpress.XtraBars.BarDockControl();
+         this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
+         this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
+         this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
+         this.createDockManagerBarButtonItem = new DevExpress.XtraBars.BarButtonItem();
+         this.dockManagerBarSubItem = new DevExpress.XtraBars.BarSubItem();
+         this.skinsBarSubItem = new DevExpress.XtraBars.SkinBarSubItem();
+         this.destroyDockManagerbarButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
+         this.dockPanelToLeftPanelBarButtonItem = new DevExpress.XtraBars.BarButtonItem();
+         this.addControlsToPanelbarButtonItem = new DevExpress.XtraBars.BarButtonItem();
+         this.clearDockManagerBarButtonItem = new DevExpress.XtraBars.BarButtonItem();
+         this.removeActiveBarButtonItem = new DevExpress.XtraBars.BarButtonItem();
+         this.hideFloatingPlanelsBarButtonItem = new DevExpress.XtraBars.BarButtonItem();
+         this.barButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
+         this.dockingPanelsBarSubItem = new DevExpress.XtraBars.BarSubItem();
+         this.dockAsTabBarButtonItem = new DevExpress.XtraBars.BarButtonItem();
+         this.DockAsSplitToBarButtonItem = new DevExpress.XtraBars.BarButtonItem();
+         this.showAutoHiddenPanelsBarButtonItem = new DevExpress.XtraBars.BarButtonItem();
+         this.barSubItem1 = new DevExpress.XtraBars.BarSubItem();
+         this.hideAutoHiddenPanelsBarButtonItem = new DevExpress.XtraBars.BarButtonItem();
+         ((System.ComponentModel.ISupportInitialize)(this.barManager1)).BeginInit();
+         this.SuspendLayout();
+         // 
+         // barManager1
+         // 
+         this.barManager1.Bars.AddRange(new DevExpress.XtraBars.Bar[] {
+            this.bar1,
+            this.bar2,
+            this.bar3});
+         this.barManager1.DockControls.Add(this.barDockControlTop);
+         this.barManager1.DockControls.Add(this.barDockControlBottom);
+         this.barManager1.DockControls.Add(this.barDockControlLeft);
+         this.barManager1.DockControls.Add(this.barDockControlRight);
+         this.barManager1.Form = this;
+         this.barManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.createDockManagerBarButtonItem,
+            this.dockManagerBarSubItem,
+            this.skinsBarSubItem,
+            this.destroyDockManagerbarButtonItem1,
+            this.dockPanelToLeftPanelBarButtonItem,
+            this.addControlsToPanelbarButtonItem,
+            this.clearDockManagerBarButtonItem,
+            this.removeActiveBarButtonItem,
+            this.hideFloatingPlanelsBarButtonItem,
+            this.barButtonItem1,
+            this.dockingPanelsBarSubItem,
+            this.dockAsTabBarButtonItem,
+            this.DockAsSplitToBarButtonItem,
+            this.showAutoHiddenPanelsBarButtonItem,
+            this.barSubItem1,
+            this.hideAutoHiddenPanelsBarButtonItem});
+         this.barManager1.MainMenu = this.bar2;
+         this.barManager1.MaxItemId = 16;
+         this.barManager1.StatusBar = this.bar3;
+         // 
+         // bar1
+         // 
+         this.bar1.BarName = "Tools";
+         this.bar1.DockCol = 0;
+         this.bar1.DockRow = 1;
+         this.bar1.DockStyle = DevExpress.XtraBars.BarDockStyle.Top;
+         this.bar1.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.dockManagerBarSubItem),
+            new DevExpress.XtraBars.LinkPersistInfo(this.dockingPanelsBarSubItem),
+            new DevExpress.XtraBars.LinkPersistInfo(this.skinsBarSubItem),
+            new DevExpress.XtraBars.LinkPersistInfo(this.barSubItem1)});
+         this.bar1.Text = "Tools";
+         // 
+         // bar2
+         // 
+         this.bar2.BarName = "Main menu";
+         this.bar2.DockCol = 0;
+         this.bar2.DockRow = 0;
+         this.bar2.DockStyle = DevExpress.XtraBars.BarDockStyle.Top;
+         this.bar2.OptionsBar.MultiLine = true;
+         this.bar2.OptionsBar.UseWholeRow = true;
+         this.bar2.Text = "Main menu";
+         // 
+         // bar3
+         // 
+         this.bar3.BarName = "Status bar";
+         this.bar3.CanDockStyle = DevExpress.XtraBars.BarCanDockStyle.Bottom;
+         this.bar3.DockCol = 0;
+         this.bar3.DockRow = 0;
+         this.bar3.DockStyle = DevExpress.XtraBars.BarDockStyle.Bottom;
+         this.bar3.OptionsBar.AllowQuickCustomization = false;
+         this.bar3.OptionsBar.DrawDragBorder = false;
+         this.bar3.OptionsBar.UseWholeRow = true;
+         this.bar3.Text = "Status bar";
+         // 
+         // barDockControlTop
+         // 
+         this.barDockControlTop.CausesValidation = false;
+         this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
+         this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
+         this.barDockControlTop.Manager = this.barManager1;
+         this.barDockControlTop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+         this.barDockControlTop.Size = new System.Drawing.Size(1065, 62);
+         // 
+         // barDockControlBottom
+         // 
+         this.barDockControlBottom.CausesValidation = false;
+         this.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+         this.barDockControlBottom.Location = new System.Drawing.Point(0, 726);
+         this.barDockControlBottom.Manager = this.barManager1;
+         this.barDockControlBottom.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+         this.barDockControlBottom.Size = new System.Drawing.Size(1065, 18);
+         // 
+         // barDockControlLeft
+         // 
+         this.barDockControlLeft.CausesValidation = false;
+         this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+         this.barDockControlLeft.Location = new System.Drawing.Point(0, 62);
+         this.barDockControlLeft.Manager = this.barManager1;
+         this.barDockControlLeft.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+         this.barDockControlLeft.Size = new System.Drawing.Size(0, 664);
+         // 
+         // barDockControlRight
+         // 
+         this.barDockControlRight.CausesValidation = false;
+         this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
+         this.barDockControlRight.Location = new System.Drawing.Point(1065, 62);
+         this.barDockControlRight.Manager = this.barManager1;
+         this.barDockControlRight.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+         this.barDockControlRight.Size = new System.Drawing.Size(0, 664);
+         // 
+         // createDockManagerBarButtonItem
+         // 
+         this.createDockManagerBarButtonItem.Caption = "Create DockManager";
+         this.createDockManagerBarButtonItem.Id = 0;
+         this.createDockManagerBarButtonItem.Name = "createDockManagerBarButtonItem";
+         this.createDockManagerBarButtonItem.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.createDockManagerBarButtonItem_ItemClick);
+         // 
+         // dockManagerBarSubItem
+         // 
+         this.dockManagerBarSubItem.Caption = "DockManager";
+         this.dockManagerBarSubItem.Id = 1;
+         this.dockManagerBarSubItem.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.createDockManagerBarButtonItem),
+            new DevExpress.XtraBars.LinkPersistInfo(this.clearDockManagerBarButtonItem),
+            new DevExpress.XtraBars.LinkPersistInfo(this.destroyDockManagerbarButtonItem1),
+            new DevExpress.XtraBars.LinkPersistInfo(this.addControlsToPanelbarButtonItem, true),
+            new DevExpress.XtraBars.LinkPersistInfo(this.removeActiveBarButtonItem),
+            new DevExpress.XtraBars.LinkPersistInfo(this.hideFloatingPlanelsBarButtonItem, true)});
+         this.dockManagerBarSubItem.Name = "dockManagerBarSubItem";
+         // 
+         // skinsBarSubItem
+         // 
+         this.skinsBarSubItem.Caption = "Skins && Themes";
+         this.skinsBarSubItem.Id = 2;
+         this.skinsBarSubItem.Name = "skinsBarSubItem";
+         // 
+         // destroyDockManagerbarButtonItem1
+         // 
+         this.destroyDockManagerbarButtonItem1.Caption = "Destroy DockManager";
+         this.destroyDockManagerbarButtonItem1.Id = 3;
+         this.destroyDockManagerbarButtonItem1.Name = "destroyDockManagerbarButtonItem1";
+         this.destroyDockManagerbarButtonItem1.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.destroyDockManagerbarButtonItem1_ItemClick);
+         // 
+         // dockPanelToLeftPanelBarButtonItem
+         // 
+         this.dockPanelToLeftPanelBarButtonItem.Caption = "Dock Panel To Left Panel";
+         this.dockPanelToLeftPanelBarButtonItem.Id = 4;
+         this.dockPanelToLeftPanelBarButtonItem.Name = "dockPanelToLeftPanelBarButtonItem";
+         this.dockPanelToLeftPanelBarButtonItem.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.dockPanelToLeftPanelBarButtonItem_ItemClick);
+         // 
+         // addControlsToPanelbarButtonItem
+         // 
+         this.addControlsToPanelbarButtonItem.Caption = "Add Controls To Panel";
+         this.addControlsToPanelbarButtonItem.Id = 5;
+         this.addControlsToPanelbarButtonItem.Name = "addControlsToPanelbarButtonItem";
+         this.addControlsToPanelbarButtonItem.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.addControlsToPanelbarButtonItem_ItemClick);
+         // 
+         // clearDockManagerBarButtonItem
+         // 
+         this.clearDockManagerBarButtonItem.Caption = "Clear DockManager";
+         this.clearDockManagerBarButtonItem.Id = 6;
+         this.clearDockManagerBarButtonItem.Name = "clearDockManagerBarButtonItem";
+         this.clearDockManagerBarButtonItem.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.clearDockManagerBarButtonItem_ItemClick);
+         // 
+         // removeActiveBarButtonItem
+         // 
+         this.removeActiveBarButtonItem.Caption = "Remove Active Panel";
+         this.removeActiveBarButtonItem.Id = 7;
+         this.removeActiveBarButtonItem.Name = "removeActiveBarButtonItem";
+         this.removeActiveBarButtonItem.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.removeActiveBarButtonItem_ItemClick);
+         // 
+         // hideFloatingPlanelsBarButtonItem
+         // 
+         this.hideFloatingPlanelsBarButtonItem.Caption = "Hide Floating Panels";
+         this.hideFloatingPlanelsBarButtonItem.Id = 8;
+         this.hideFloatingPlanelsBarButtonItem.Name = "hideFloatingPlanelsBarButtonItem";
+         this.hideFloatingPlanelsBarButtonItem.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.hideFloatingPlanelsBarButtonItem_ItemClick);
+         // 
+         // barButtonItem1
+         // 
+         this.barButtonItem1.Caption = "barButtonItem1";
+         this.barButtonItem1.Id = 9;
+         this.barButtonItem1.Name = "barButtonItem1";
+         // 
+         // dockingPanelsBarSubItem
+         // 
+         this.dockingPanelsBarSubItem.Caption = "Dock Panels";
+         this.dockingPanelsBarSubItem.Id = 10;
+         this.dockingPanelsBarSubItem.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.dockPanelToLeftPanelBarButtonItem),
+            new DevExpress.XtraBars.LinkPersistInfo(this.dockAsTabBarButtonItem),
+            new DevExpress.XtraBars.LinkPersistInfo(this.DockAsSplitToBarButtonItem)});
+         this.dockingPanelsBarSubItem.Name = "dockingPanelsBarSubItem";
+         // 
+         // dockAsTabBarButtonItem
+         // 
+         this.dockAsTabBarButtonItem.Caption = "Dock As Tab";
+         this.dockAsTabBarButtonItem.Id = 11;
+         this.dockAsTabBarButtonItem.Name = "dockAsTabBarButtonItem";
+         // 
+         // DockAsSplitToBarButtonItem
+         // 
+         this.DockAsSplitToBarButtonItem.Caption = "Dock To (as Split)";
+         this.DockAsSplitToBarButtonItem.Id = 12;
+         this.DockAsSplitToBarButtonItem.Name = "DockAsSplitToBarButtonItem";
+         this.DockAsSplitToBarButtonItem.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.DockAsSplitToBarButtonItem_ItemClick);
+         // 
+         // showAutoHiddenPanelsBarButtonItem
+         // 
+         this.showAutoHiddenPanelsBarButtonItem.Caption = "Show Panels";
+         this.showAutoHiddenPanelsBarButtonItem.Id = 13;
+         this.showAutoHiddenPanelsBarButtonItem.Name = "showAutoHiddenPanelsBarButtonItem";
+         this.showAutoHiddenPanelsBarButtonItem.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.showAutoHiddenPanelsBarButtonItem_ItemClick);
+         // 
+         // barSubItem1
+         // 
+         this.barSubItem1.Caption = "Auto Hidden Panels";
+         this.barSubItem1.Id = 14;
+         this.barSubItem1.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.showAutoHiddenPanelsBarButtonItem),
+            new DevExpress.XtraBars.LinkPersistInfo(this.hideAutoHiddenPanelsBarButtonItem)});
+         this.barSubItem1.Name = "barSubItem1";
+         // 
+         // hideAutoHiddenPanelsBarButtonItem
+         // 
+         this.hideAutoHiddenPanelsBarButtonItem.Caption = "Hide Panels";
+         this.hideAutoHiddenPanelsBarButtonItem.Id = 15;
+         this.hideAutoHiddenPanelsBarButtonItem.Name = "hideAutoHiddenPanelsBarButtonItem";
+         this.hideAutoHiddenPanelsBarButtonItem.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.hideAutoHiddenPanelsBarButtonItem_ItemClick);
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(1065, 744);
+         this.Controls.Add(this.barDockControlLeft);
+         this.Controls.Add(this.barDockControlRight);
+         this.Controls.Add(this.barDockControlBottom);
+         this.Controls.Add(this.barDockControlTop);
+         this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+         this.Name = "Form1";
+         this.Text = "Form1";
+         ((System.ComponentModel.ISupportInitialize)(this.barManager1)).EndInit();
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private DevExpress.XtraBars.BarManager barManager1;
+      private DevExpress.XtraBars.Bar bar1;
+      private DevExpress.XtraBars.Bar bar2;
+      private DevExpress.XtraBars.Bar bar3;
+      private DevExpress.XtraBars.BarDockControl barDockControlTop;
+      private DevExpress.XtraBars.BarDockControl barDockControlBottom;
+      private DevExpress.XtraBars.BarDockControl barDockControlLeft;
+      private DevExpress.XtraBars.BarDockControl barDockControlRight;
+      private DevExpress.XtraBars.BarButtonItem createDockManagerBarButtonItem;
+      private DevExpress.XtraBars.BarSubItem dockManagerBarSubItem;
+      private DevExpress.XtraBars.SkinBarSubItem skinsBarSubItem;
+      private DevExpress.XtraBars.BarButtonItem destroyDockManagerbarButtonItem1;
+      private DevExpress.XtraBars.BarButtonItem dockPanelToLeftPanelBarButtonItem;
+      private DevExpress.XtraBars.BarButtonItem addControlsToPanelbarButtonItem;
+      private DevExpress.XtraBars.BarButtonItem clearDockManagerBarButtonItem;
+      private DevExpress.XtraBars.BarButtonItem removeActiveBarButtonItem;
+      private DevExpress.XtraBars.BarButtonItem hideFloatingPlanelsBarButtonItem;
+      private DevExpress.XtraBars.BarSubItem dockingPanelsBarSubItem;
+      private DevExpress.XtraBars.BarButtonItem barButtonItem1;
+      private DevExpress.XtraBars.BarButtonItem dockAsTabBarButtonItem;
+      private DevExpress.XtraBars.BarButtonItem DockAsSplitToBarButtonItem;
+      private DevExpress.XtraBars.BarSubItem barSubItem1;
+      private DevExpress.XtraBars.BarButtonItem showAutoHiddenPanelsBarButtonItem;
+      private DevExpress.XtraBars.BarButtonItem hideAutoHiddenPanelsBarButtonItem;
+   }
+}
+
---
+++ DockManagerTest/Form1.cs
@@ -0,0 +1,234 @@
+﻿using DevExpress.XtraBars.Docking;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DockManagerTest
+{
+   public partial class Form1 : DevExpress.XtraEditors.XtraForm
+   {
+      public Form1()
+      {
+         InitializeComponent( );
+      }
+
+      private DockManager _dm;
+      public DockManager dm
+      {
+         get
+         {
+            if( this._dm == null )
+            {
+               // Create a dock manager and initialize its Form property. 
+               this._dm = new DockManager( this );
+            }
+            return this._dm;
+         }
+      }
+
+      private void createDockManagerBarButtonItem_ItemClick( object sender, DevExpress.XtraBars.ItemClickEventArgs e )
+      {
+         // Create and dock a panel to the top edge. 
+         DockPanel panelTop = this.dm.AddPanel( DockingStyle.Top );
+         panelTop.Text = "#Top";
+         DockPanel panelBottom = this.dm.AddPanel( DockingStyle.Bottom );
+         panelBottom.Text = "#Bottom";
+         DockPanel panelRight = this.dm.AddPanel( DockingStyle.Right );
+         panelRight.Text = "#Right";
+         DockPanel panelLeft = this.dm.AddPanel( DockingStyle.Left );
+         panelLeft.Text = "#Left";
+         DockPanel panelFloat = this.dm.AddPanel( DockingStyle.Float );
+         panelFloat.Text = "Float #1";
+      }
+
+      private void clearDockManagerBarButtonItem_ItemClick( object sender, DevExpress.XtraBars.ItemClickEventArgs e )
+      {
+         if( this._dm != null )
+         {
+            this._dm.Clear( );
+         }
+      }
+
+      private void destroyDockManagerbarButtonItem1_ItemClick( object sender, DevExpress.XtraBars.ItemClickEventArgs e )
+      {
+         if( this._dm != null )
+         {
+            this._dm.Dispose( );
+            this._dm = null;
+         }
+      }
+
+      private void dockPanelToLeftPanelBarButtonItem_ItemClick( object sender, DevExpress.XtraBars.ItemClickEventArgs e )
+      {
+         // Create a panel and dock it to the form's right edge. 
+         DockPanel panel1 = this.dm.AddPanel( DockingStyle.Right );
+         panel1.Text = "Panel 1";
+         // Add a new panel to panel1. This forms a split container that owns panel1 and panel2. 
+         DockPanel panel2 = panel1.AddPanel( );
+         panel2.Text = "Panel 2";
+         // Transform the split container into a tab container. 
+         DockPanel container = panel1.ParentPanel;
+         container.Tabbed = true;
+      }
+
+      private void addControlsToPanelbarButtonItem_ItemClick( object sender, DevExpress.XtraBars.ItemClickEventArgs e )
+      {
+         // Create dock panels, panel1 and panel2 form a split container. 
+         DockPanel panel1 = this.dm.AddPanel( DockingStyle.Top );
+         panel1.Text = "Panel 1";
+         DockPanel panel2 = panel1.AddPanel( );
+         panel2.Text = "Panel 2";
+
+         // Place a PropertyGrid control on panel1. 
+         PropertyGrid pGrid = new PropertyGrid( );
+         pGrid.SelectedObject = panel1;
+         pGrid.Dock = DockStyle.Fill;
+         panel1.ControlContainer.Controls.Add( pGrid );
+
+         // Place a ListBox control on panel2. 
+         ListBox lBox = new ListBox( );
+         lBox.Items.AddRange( new string[ ] { "Item 1", "Item 2", "Item 3" } );
+         lBox.Dock = DockStyle.Fill;
+         panel2.ControlContainer.Controls.Add( lBox );
+      }
+
+      private void removeActiveBarButtonItem_ItemClick( object sender, DevExpress.XtraBars.ItemClickEventArgs e )
+      {
+         this.dm.RemovePanel( this.dm.ActivePanel );
+      }
+
+      private void hideFloatingPlanelsBarButtonItem_ItemClick( object sender, DevExpress.XtraBars.ItemClickEventArgs e )
+      {
+         bool isToHide = string.Compare( this.hideFloatingPlanelsBarButtonItem.Caption, "Hide Floating Panels", StringComparison.Ordinal ) == 0;
+         if( isToHide )
+         {
+            int index = 0;
+            // Iterate through the visible panels which are not owned by other panels. 
+            while( index < this.dm.RootPanels.Count )
+            {
+               DockPanel panel = this.dm.RootPanels[ index ];
+               // Hide the panel if it's floating. 
+               if( panel.FloatForm == null )
+                  index++;
+               else
+                  panel.Hide( );
+            }
+            this.hideFloatingPlanelsBarButtonItem.Caption = "Show Floating Panels";
+         }
+         else
+         {
+            int count = this.dm.AutoHideContainers.Count;
+            // Iterate through the visible panels which are not owned by other panels. 
+            while( this.dm.HiddenPanels.Count > 0 )
+            {
+               DockPanel panel = this.dm.HiddenPanels[ 0 ];
+               panel.Show( );
+               //switch( panel.Visibility )
+               //{
+               //   case DockVisibility.Visible:
+               //      break;
+               //   case DockVisibility.Hidden:
+               //      break;
+               //   case DockVisibility.AutoHide:
+               //      break;
+               //   default:
+               //      break;
+               //}
+            }
+            this.hideFloatingPlanelsBarButtonItem.Caption = "Hide Floating Panels";
+         }
+      }
+
+      private void DockAsSplitToBarButtonItem_ItemClick( object sender, DevExpress.XtraBars.ItemClickEventArgs e )
+      {
+         //// create a tab container with panel1 and panel2 tabs 
+         //panel2.DockAsTab( panel1, 1 );
+
+         ////or 
+
+         //// create a split container with two panels 
+         //panel2.DockTo( panel1 );
+         //// transform a split container into a tab container 
+         //panel1.ParentPanel.Tabbed = true;
+      }
+
+      private void showAutoHiddenPanelsBarButtonItem_ItemClick( object sender, DevExpress.XtraBars.ItemClickEventArgs e )
+      {
+         showTopAutoHiddenDockPanels( );
+         showLeftAutoHiddenDockPanels( );
+         showBottomAutoHiddenDockPanels( );
+         showRightAutoHiddenDockPanels( );
+         showFloatAutoHiddenDockPanels( );
+         showFillAutoHiddenDockPanels( );
+      }
+      private void showTopAutoHiddenDockPanels()
+      {
+         AutoHideContainer container = this.dm.AutoHideContainers[ DockingStyle.Top ];
+         makeVisible( container );
+      }
+      private void showLeftAutoHiddenDockPanels()
+      {
+         AutoHideContainer container = this.dm.AutoHideContainers[ DockingStyle.Left ];
+         makeVisible( container );
+      }
+      private void showBottomAutoHiddenDockPanels()
+      {
+         AutoHideContainer container = this.dm.AutoHideContainers[ DockingStyle.Bottom ];
+         makeVisible( container );
+      }
+      private void showRightAutoHiddenDockPanels()
+      {
+         AutoHideContainer container = this.dm.AutoHideContainers[ DockingStyle.Right ];
+         makeVisible( container );
+      }
+      private void showFloatAutoHiddenDockPanels()
+      {
+         AutoHideContainer container = this.dm.AutoHideContainers[ DockingStyle.Float ];
+         makeVisible( container );
+      }
+      private void showFillAutoHiddenDockPanels()
+      {
+         AutoHideContainer container = this.dm.AutoHideContainers[ DockingStyle.Fill ];
+         makeVisible( container );
+      }
+
+      private static void makeVisible( AutoHideContainer container )
+      {
+         if( container != null )
+         {
+            while( container.Count > 0 )
+            {
+               DockPanel dp = container[ container.Count - 1 ];
+               dp.Visibility = DockVisibility.Visible;
+            }
+         }
+      }
+      private static void makeHidden( AutoHideContainer container )
+      {
+         if( container != null )
+         {
+            while( container.Count > 0 )
+            {
+               DockPanel dp = container[ container.Count - 1 ];
+               dp.Visibility = DockVisibility.Hidden;
+            }
+         }
+      }
+      private static void makeAutoHide( AutoHideContainer container )
+      {
+         if( container != null )
+         {
+            while( container.Count > 0 )
+            {
+               DockPanel dp = container[ container.Count - 1 ];
+               dp.Visibility = DockVisibility.AutoHide;
+            }
+         }
+      }
+
+      private void hideAutoHiddenPanelsBarButtonItem_ItemClick( object sender, DevExpress.XtraBars.ItemClickEventArgs e )
+      {
+
+      }
+   }
+}
